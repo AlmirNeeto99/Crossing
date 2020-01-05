0e3c27a504525f40259c2b4d8675fe4abf7762c8
= <=>
2020-01-05 03:50:12 -0300

    .

--- src/Model/Limits/Limits.java
+++
@@ -1,9 +0,0 @@
-package Model.Limits;
-
-public abstract class Limits {
-
-    public final int[] up = {240, 71};
-    public final int[] down = {320, 525};
-    public final int[] left = {71, 320};
-    public final int[] right = {525, 240};
-}
